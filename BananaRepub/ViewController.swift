04d4aaca9cfdaa7e0a96ae3efeac4f68039026dd
Derek Hunten <dhunten@Dereks-MBP.hsd1.ca.comcast.net>
2014-08-17 13:20:51 -0700

    Initial Commit

---
+++ BananaRepub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BananaRepub
+//
+//  Created by Derek Hunten on 8/17/14.
+//  Copyright (c) 2014 Derek Hunten. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
