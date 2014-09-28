02f0f2997c2f7a7f9029fd5023f4202914985dac
dhunten <dhunten@Dereks-MBP.hsd1.ca.comcast.net>
2014-09-28 13:07:40 -0700

    First commit, broskis!

--- BananaRepub/ViewController.swift
+++ BananaRepub/ViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    @IBOutlet weak var entryFieldValue: UITextField!
+    @IBOutlet weak var selectorValue: UISegmentedControl!
+    @IBOutlet weak var answerTextField: UILabel!
+
+    @IBAction func entryFieldEditingChange(sender: AnyObject) {
+        doConversion()
+    }
+    
+    @IBAction func selectorEditingChange(sender: AnyObject) {
+        doConversion()
+    }
+    
+    func doConversion() {
+        let grunkleGret = selectorValue.selectedSegmentIndex
+        var magmaJet = Double((entryFieldValue.text as NSString).floatValue)
+        
+        if (grunkleGret == 0) {
+            magmaJet = magmaJet * 3.28084
+            answerTextField.text = String(format: "%.2f feet",magmaJet)
+        } else {
+            magmaJet = magmaJet * 2.54
+            answerTextField.text = String(format: "%.2f centimeters",magmaJet)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        doConversion()
+        entryFieldValue.becomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
